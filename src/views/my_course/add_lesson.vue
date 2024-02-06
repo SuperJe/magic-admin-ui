f4609a1f61a02cde4c560abad0dbf865ea34c16f
Will <958953720@qq.com>
2024-02-06 09:50:28 +0800

    feat: add_lesson页面增加学生名字搜索功能

--- src/views/my_course/add_lesson.vue
+++ src/views/my_course/add_lesson.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-      <el-form-item label="学生名字" type="textarea" prop="name">
-        <el-input v-model="form.name" />
+      <el-form-item label="学生名字" prop="name">
+        <el-autocomplete
+          v-model="form.name"
+          :fetch-suggestions="querySearchAsync"
+          placeholder="请输入内容"
+          @select="handleSelect"
+        />
       </el-form-item>
       <el-form-item label="课堂记录" prop="record">
         <el-input v-model="form.record" type="textarea" autosize />
@@ -19,8 +24,9 @@
       </el-form-item>
       <el-form-item label="授课老师" prop="teacher">
         <el-select v-model="form.teacher" placeholder="请选择授课老师">
-          <el-option label="颜老师" value="颜老师" />
           <el-option label="岳老师" value="岳老师" />
+          <el-option label="颜老师" value="颜老师" />
+          <el-option label="郭老师" value="郭老师" />
         </el-select>
       </el-form-item>
       <el-form-item label="课程类型" prop="courseType">
@@ -31,7 +37,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">提交</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="resetForm('form')">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -39,25 +45,25 @@
 
 <script>
 import { submitAddLesson } from '@/api/admin/course'
+import { listUser } from '@/api/admin/sys-user'
 
 export default {
   data() {
     return {
-      students: [],
-      state: '',
-      timeout: null,
       form: {
+        students: [],
         name: '',
+        timeout: null,
         record: '',
         date: '',
         knowledgeTags: '',
-        teacher: '颜老师',
+        teacher: '岳老师',
         courseType: '2'
       },
       rules: {
         name: [
-          { required: true, message: '请输入学生名字', trigger: 'blur' },
-          { min: 2, max: 5, message: '长度在 2 到 5 个字符', trigger: 'blur' }
+          { required: true, message: '请输入学生名字', trigger: 'change' },
+          { min: 2, max: 5, message: '长度在 2 到 5 个字符', trigger: 'change' }
         ],
         record: [
           { required: true, message: '课堂记录不能为空', trigger: 'blur' }
@@ -74,7 +80,16 @@ export default {
         courseType: [
           { required: true, message: '请选择课程类型', trigger: 'change' }
         ]
-      }
+      },
+      queryParams: {
+        pageIndex: 1,
+        pageSize: 10,
+        username: undefined,
+        phone: undefined,
+        status: undefined,
+        deptId: undefined
+      },
+      users: []
     }
   },
   mounted() {
@@ -82,6 +97,7 @@ export default {
   },
   methods: {
     onSubmit() {
+      console.log('submit')
       this.$refs.form.validate(valid => {
         if (valid) {
           const req = {
@@ -89,22 +105,31 @@ export default {
             tags: this.form.knowledgeTags, teacher: this.form.teacher, course_type: Number(this.form.courseType)
           }
           submitAddLesson(JSON.stringify(req)).then(response => {
-            if (response.code !== 200 || response.data.code !== 0) {
-              alert('提交失败！' + response.data.msg)
-            } else {
-              alert('提交成功: ' + response.data.msg + ', id=' + response.data.id)
-            }
             console.log(response)
+            if (response.code === 200 && response.data.code === 0) {
+              this.$alert('提交成功', '提交信息', {
+                confirmButtonText: '确定'
+              })
+            }
           })
+
+          console.log('Form submitted:', this.form)
         } else {
           console.log('Validation failed')
         }
       })
     },
+    resetForm() {
+      this.$refs.form.resetFields()
+    },
     loadAll() {
-      return [
-        { 'value': '小明' }
-      ]
+      listUser(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+        this.total = response.data.count
+        for (let i = 0; i < this.total; i++) {
+          this.users[i] = { 'value': response.data.list[i].username }
+        }
+      })
+      return this.users
     },
     querySearchAsync(queryString, cb) {
       var students = this.students
@@ -116,8 +141,8 @@ export default {
       }, 3000 * Math.random())
     },
     createStateFilter(queryString) {
-      return (state) => {
-        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+      return (name) => {
+        return (name.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
       }
     },
     handleSelect(item) {
